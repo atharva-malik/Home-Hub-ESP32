aca780120f4c1da7cf6204acb708c37d83699e44
Atharva <atharva.malik@gmail.com>
2024-08-16 19:56:27 +1000

    Implemented the slider and almost finished the bar graph

--- Home Hub/main.py
+++ Home Hub/main.py
@@ -13,9 +13,37 @@
 # Buzzer: To tell me if smth is wrong
 # Button: To reset session
 
+# Generic Imports
+from machine import Pin, ADC
+import time
+
+# Slider setup
+p4 = Pin(4, Pin.IN)
+sl = ADC(p4)
+sl.atten(ADC.ATTN_11DB) # This is to make it work with the 3.3v power supply
+sliderVal = sl.read() # Ranges from 0-4095 (inclusive)
+
+# Led Bar Graph setup
+barGraphPins = [Pin(46, Pin.OUT), Pin(3, Pin.OUT), Pin(8, Pin.OUT), Pin(18, Pin.OUT), Pin(17, Pin.OUT), Pin(16, Pin.OUT), Pin(15, Pin.OUT), Pin(7, Pin.OUT), Pin(6, Pin.OUT), Pin(5, Pin.OUT)]
+
 # Planning out the variables:
 cTemp = 0 # Current Temperature
 cHum = 0 # Current Humidity
 timeLeft = 0 # Time Left in Session
 sesLen = 0 # Session length
 
+# Generic functions
+def map_value(value, range1_min, range1_max, range2_min, range2_max):
+    ratio = (value - range1_min) / (range1_max - range1_min)
+    mapped_value = ratio * (range2_max - range2_min) + range2_min
+    return int(mapped_value)
+
+while True:
+  sliderVal = map_value(sl.read(), 0, 4095, 0, 10)
+#   for i in barGraphPins:
+#     i.value(1)
+#     time.sleep(1)
+  print()
+#   for i in barGraphPins:
+#     i.value(0)
+#     time.sleep(1)
